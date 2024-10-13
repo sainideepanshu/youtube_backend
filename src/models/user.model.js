28861b56c09b5853520b251cc91ddc5efd9bd96d
deepanshu <deepanshukamlesh@gamil.com>
2024-10-14 01:39:29 +0530

    User and Video model with hooks and jwt and bcrypt and mongoose-aggregate-paginate-v2

---
+++ src/models/user.model.js
@@ -0,0 +1,101 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+//Pre middleware functions are executed one after another, when each middleware calls next.
+//Jaise hi humara data save hone jaara hoga just usse pehle hum iss pre hook ko run karwa sakte hai
+//Note : jab bhi iss pre hook k andar hum calllback function likhte hai tu aise hani likhna : () => {} , 
+// because javascript mai hamare pass 'this' ka reference nahi hota i.e. context nahi pat hota hai usse
+// yaha par context ka pata hona bhot zaruri hi kyuki save event iss user par chal raha hai
+// ye encryption algo time leti hai so it is an async process
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+//we can design our own custom methods
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)   // it will return true or false
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
